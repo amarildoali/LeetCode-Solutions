e74ac9b93c73401b3b29f7fa0908a0cec83f6fdc
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2022-11-24 19:58:27 +0100

    add: 1486

---
+++ Solutions/1486_XOR_Operation_in_an_Array.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums = []
+        for i in range(n):
+            nums.append(start + 2 * i)
+        return reduce(lambda a, b: a ^ b, nums)
