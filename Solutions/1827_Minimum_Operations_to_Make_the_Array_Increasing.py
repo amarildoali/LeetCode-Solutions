384df20a7719d570b775e36b30fb12e378e20ac6
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2022-12-06 17:12:33 +0100

    fix: 1827

--- Solutions/1827_Minimum_Operations_to_Make_the_Array_Increasing.py
+++ Solutions/1827_Minimum_Operations_to_Make_the_Array_Increasing.py
@@ -1,11 +1,12 @@
 from typing import List
 
 
-def minOperations(self, nums: List[int]) -> int:
-    result = 0
-    for i in range(len(nums) - 1):
-        if nums[i] >= nums[i + 1]:
-            diff = nums[i] - nums[i + 1]
-            nums[i + 1] += diff + 1
-            result += diff + 1
-    return result
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        result = 0
+        for i in range(len(nums) - 1):
+            if nums[i] >= nums[i + 1]:
+                diff = nums[i] - nums[i + 1]
+                nums[i + 1] += diff + 1
+                result += diff + 1
+        return result
