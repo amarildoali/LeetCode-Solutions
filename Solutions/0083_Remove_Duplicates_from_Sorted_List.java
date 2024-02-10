4dd3b24ed58af923e2de2f7a4fb66ec132378007
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2024-02-11 00:58:58 +0100

    add: 21

--- Solutions/0083_Remove_Duplicates_from_Sorted_List.java
+++ Solutions/0083_Remove_Duplicates_from_Sorted_List.java
@@ -1,11 +1,11 @@
 package Solutions;
 
 class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
 class Solution {
