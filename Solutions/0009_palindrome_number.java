fd2dd3c4babe2e1d7c0acae991b4fef376c83879
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2024-03-01 00:25:11 +0100

    add: Problem 9

---
+++ Solutions/0009_palindrome_number.java
@@ -0,0 +1,16 @@
+package Solutions;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int head = 0;
+        int tail = s.length() - 1;
+
+        while (head < tail) {
+            if (s.charAt(head++) != s.charAt(tail--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
